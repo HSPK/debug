309a2a5021c24a72115a9b42a5f5dac6ffc82c9e
whxway <whxway@qq.com>
2020-06-25 20:21:58 +0800

    Finish debugging folder 10

--- 10/4.c
+++ 10/4.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-int main() {
-int num;
-    scanf("%d",num);
+int main()
+{
+    int num;
+    scanf("%d", &num);
     int digit;
     int a;
-    for(digit=2;digit<=num;digit++)
-    {
-        for(a=2;digit<num;a++) {
-            if(digit % a ==0) {
+
+    for (digit = 2; digit <= num; digit++) {
+        int c = 0;
+        for (a = 2; a * a <= digit; a++) {
+            if (digit % a == 0) {
+                c = 1;
                 break;
             }
-        
-            }
         }
-    if(a==digit) {
-        printf("digit/n");
+        if (c == 0) {
+            printf("%d\n", digit);
+        }
     }
+
     return 0;
 }
