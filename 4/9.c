5718b39373ce98ef8ebeb9aa49804252fb526786
xiaohei <614391147@qq.com>
2020-06-26 14:22:48 +0800

    DEBUG

--- 4/9.c
+++ 4/9.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void swap(int a, int b) {
+void swap(int *a, int *b) {
     int c;
-    if(a <= b){
-        c = a;
-        a = b;
-        b = c;
+    if(*a <= *b){
+        c = *a;
+        *a = *b;
+        *b = c;
     }
 }
 
@@ -15,18 +15,19 @@ int main() {
     int numbers[10];
     int i;
     
-    // 读入给定的数字
     for (i = 0; i < n; i++) {
         scanf("%d", &numbers[i]);
     }
     for (i = 0; i < n; i++) {
-        for(int j = 0; j < n - 1; j++) {
-            swap(numbers[j], numbers[j + 1]);
+        for(int j = 0; j < n - 1 - i; j++) {
+            swap(&numbers[j], &numbers[j + 1]);
         }
     }
     for (int i = 0; i < n; i++) {
         printf("%d", numbers[i]);
-        if(i != n - 1) printf(" ");
+        if(i != n - 1) {
+            printf(" ");
+        }
     }
     
 
