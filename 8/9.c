a2c5a92959a80695e6329c1c3b1fdfe6d845f480
linqiqi <614391147@qq.com>
2020-06-27 01:34:43 +0800

    8DEBUG

--- 8/9.c
+++ 8/9.c
@@ -4,11 +4,11 @@ int main() {
     int n;
     int i, j;
     scanf("%d\n", &n);
-    char string[n][101];
-    for (i = 0; i < n; i++){
-        for (j = 0; j < 100; j++){
+    char string[100][101];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < 101; j++) {
             scanf("%c", &string[i][j]);
-            if (string[i][j] == '\n'){
+            if (string[i][j] == '\n') {
                 string[i][j] = '\0';
                 break;
             }
@@ -22,6 +22,6 @@ int main() {
             strcpy(string[i+1],str);
         }
     }
-    printf("%s", string[n-1]);
+    printf("%s\n", string[n-1]);
     return 0;
 }
