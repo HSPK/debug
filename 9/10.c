70210dbb02088189405b7da9e799a83781b3a966
linqiqi <614391147@qq.com>
2020-06-28 15:15:37 +0800

    DEBUG 9

--- 9/10.c
+++ 9/10.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-int f(int n){
-    if(n<=3){return 1;}
-    else{return f(n-2)+f(n-3);}
+int f(int n)
+{
+    if(n <= 3 && n > 1){
+        return 1;
+    } else if(n <= 1) {
+        return 0;
+    } else {
+        return f(n - 2) + f(n - 3);
+    }
 }
 int main() {
     int n;
--- 9/8.c
+++ 9/8.c
@@ -2,19 +2,14 @@
 
 int main() {
     int n;
-    scanf("%d",&n);
-    int i=0;
-    int x;
-    int y;
-    int m=1;
-    int b=1;
-    for(x = 0; x <= n / 2; x++){
-        for(y = 0; y <= n / 3; y++){
-            if(x * 2 + y * 3 == n){
-                i++;
-            }
-        }
+    scanf("%d", &n);
+    int i = 0;
+    int part[51] = { 0 };
+    part[2] = 1;
+    part[3] = 1;
+    for(i = 4; i <= n; i++) {
+        part[i] = part[i - 2] + part[i - 3];
     }
-    printf("%d",i);
+    printf("%d",part[n]);
     return 0;
 }
