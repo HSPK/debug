3b0cb161de49f459f5b912b030dcaba86675be41
linqiqi <614391147@qq.com>
2020-06-28 20:21:11 +0800

    DEBUG 10

--- 10/6.c
+++ 10/6.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-int main() {
-int a,b,c;
-    scanf("%d",&c);
-        
-     for(b=2;b<=c;b++) { 
-     for(a=2;a<b;a++){
-     if(b%a==0){printf("");break;}
-     }
-        if (a>=b){printf("%d\n"),b;}
+int main() 
+{
+    int a, b, c;
+    scanf("%d", &c);    
+    for (b = 2; b <= c; b++) { 
+        for (a = 2; a < b; a++) {
+            if (b % a == 0) {
+                break;
+            }
+        }
+        if (a >= b) {
+            printf("%d\n", b);
+        }
      }
     return 0;
 }
