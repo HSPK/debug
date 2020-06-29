90208362aa9c61c47673ea80c4f224e30bd13311
linqiqi <614391147@qq.com>
2020-06-30 02:53:08 +0800

    DEBUG 3

--- 3/10.c
+++ 3/10.c
@@ -7,60 +7,59 @@ int main() {
 
     int k = 0;
     int l = 0;
-    int num;
+    int num = 0;
     scanf("%d %d", &m, &n);
-    
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            scanf("%d ", &matrix[i][j]);
+    int sum = m * n; 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
         }
     }
 
       
-    while (k < m && l < n)
-    {
+    while (k < m && l < n) {
         /* Print the first row from the remaining rows */
-        for (int i = l; i < n; i++)
-        {
-            printf("%d ", matrix[k][i]);
-
+        for (int i = l; i < n; i++) {
+            printf("%d", matrix[k][i]);
+            num++;
+            if (num != sum) {
+                printf(" ");
+            }
         }
         k++;
- 
         /* Print the last column from the remaining columns */
-        for (int i = k; i < m; i++)
-        {
-            printf("%d ", matrix[i][n - 1]);
-
+        for (int i = k; i < m; i++) {
+            printf("%d", matrix[i][n - 1]);
+            num++;
+            if (num != sum) {
+                printf(" ");
+            }
         }
         n--;
- 
         /* Print the last row from the remaining rows */
-        if (k < m)
-        {
-            for (int i = n - 1; i >= l; i--)
-            {
-                printf("%d ", matrix[m - 1][i]);
- 
+        if (k < m) {
+            for (int i = n - 1; i >= l; i--) {
+                printf("%d", matrix[m - 1][i]);
+                num++;
+                if (num != sum) {
+                    printf(" ");
+                }
+                else return 0;
             }
             m--;
         }
- 
         /* Print the first column from the remaining columns */
-        if (l < n)
-        {
-            for (int i = m - 1; i >= k; i--)
-            {
+        if (l < n) {
+            for (int i = m - 1; i >= k; i--) {
                 printf("%d", matrix[i][l]);
-                if (i != k){
+                num++;
+                if (num != sum) {
                     printf(" ");
-            }
-
+                }
+                else return 0;
             }
             l++;    
         }        
     }
-
-
     return 0;
 }
