cd33afdfee1d640f8c1ac4d872ea0f8d2d94a775
linqiqi <614391147@qq.com>
2020-06-30 08:23:59 +0800

    DEBUG 3

--- 3/8.c
+++ 3/8.c
@@ -10,55 +10,55 @@ int main()
     int max;
     
     //input m & n & matrix
-	scanf("%d %d", &m, &n);
-	for (i = 0; i < m; i++) {
-		for (j = 0; j < n; j++) {
-			scanf("%d", &matrix[i][j]);
-		}
-	}
-	max = m * n;
-	
-	//output the first line
-	for (j = 0;j < n; j++) {
-		printf("%d", matrix[0][j]);
-		count++;
-		if (count < max) {
-			printf(" ");
-		} else {
-			return 0;
-		}
-	}
-		
-	do {	
-		int matrix_[100][100];//rotate the matrix to -90 degree which becomes matrix_
-		for (i = 0; i < n; i++) {
-			for (j = 0; j < m - 1; j++) {
-				matrix_[i][j] = matrix[j + 1][n - 1 - i];
-			}
-		}
-		
-		for (j = 0; j < m - 1; j++) {
-			printf("%d", matrix_[0][j]);
-			count++;
-			if(count < max) {
-				printf(" ");
-			} else {
-				return 0;
-			}
-		}
-	
-		//cover original matrix with new matrix_	
-		for (i = 0; i < n; i++) {
-			for (j = 0; j < m; j++) {
-				matrix[i][j] = matrix_[i][j];
-			}
-		}
-		
-		int t;
-		t = m;
-		m = n;
-		n = t-1;
-	} while(m >= 1 && n >= 1);
-	
+    scanf("%d %d", &m, &n);
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    max = m * n;
+    
+    //output the first line
+    for (j = 0;j < n; j++) {
+        printf("%d", matrix[0][j]);
+        count++;
+        if (count < max) {
+            printf(" ");
+        } else {
+            return 0;
+        }
+    }
+        
+    do {    
+        int matrix_[100][100];//rotate the matrix to -90 degree which becomes matrix_
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m - 1; j++) {
+                matrix_[i][j] = matrix[j + 1][n - 1 - i];
+            }
+        }
+        
+        for (j = 0; j < m - 1; j++) {
+            printf("%d", matrix_[0][j]);
+            count++;
+            if(count < max) {
+                printf(" ");
+            } else {
+                return 0;
+            }
+        }
+    
+        //cover original matrix with new matrix_    
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                matrix[i][j] = matrix_[i][j];
+            }
+        }
+        
+        int t;
+        t = m;
+        m = n;
+        n = t-1;
+    } while(m >= 1 && n >= 1);
+    
     return 0;
 }
