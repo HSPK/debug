09c2118a9c51a0713701051784a50e92f0fcbbef
linqiqi <614391147@qq.com>
2020-06-30 21:28:15 +0800

    Fixed 11

--- 11/10.c
+++ 11/10.c
@@ -16,14 +16,14 @@ int main()
         i++;
     }
     k = strlen(str);
-    for(i = 1; i <= k; i++) {
+    for (i = 1; i <= k; i++) {
         arr[i % 32] += (int)str[i - 1];
     }
-    for(j = 0; j < 32; j++) {
+    for (j = 0; j < 32; j++) {
         bits[j] = arr[31 - j] ^ (arr[j] << 1);
         rstr[j] =(char)(bits[j] % 85 + 34);
     }
-    for(i = 0; i < 32; i++) {
+    for (i = 0; i < 32; i++) {
         printf("%c", rstr[i]);
     }
     return 0;
